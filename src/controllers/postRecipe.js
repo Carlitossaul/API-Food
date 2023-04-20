fbe7c27f7242b489f6a6f5e26f1c1c75dc77ad94
Carlitossaul <carlitossaul.h@gmail.com>
2023-04-20 14:55:46 -0300

    s

--- src/controllers/postRecipe.js
+++ src/controllers/postRecipe.js
@@ -1,5 +1,4 @@
 const { Recipe, Diet } = require("../db");
-const { Op } = require("sequelize");
 
 const postRecipe = async (
   name,
@@ -11,26 +10,22 @@ const postRecipe = async (
   readyInMinutes,
   servings
 ) => {
-  try {
-    const recipe = await Recipe.create({
-      name,
-      image,
-      summary,
-      healthScore,
-      steps,
-      servings,
-      readyInMinutes,
+  const recipe = await Recipe.create({
+    name,
+    image,
+    summary,
+    healthScore,
+    steps,
+    servings,
+    readyInMinutes,
+  });
+  if (diets && diets.length > 0) {
+    const dietDb = await Diet.findAll({
+      where: { name: diets },
     });
-    if (diets && diets.length > 0) {
-      const dietDb = await Diet.findAll({
-        where: { name: diets },
-      });
-      recipe.addDiet(dietDb);
-    }
-    return recipe;
-  } catch (error) {
-    throw new Error("Could not create recipe: " + error.message);
+    recipe.addDiet(dietDb);
   }
+  return recipe;
 };
 
 module.exports = { postRecipe };
