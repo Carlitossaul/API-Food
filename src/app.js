045a6579c1c2c1ca544392c92f6b188968f72eb8
Carlitossaul <carlitossaul.h@gmail.com>
2023-05-05 14:32:59 -0300

    s

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
   //https://areyouhungry.vercel.app
-  res.header("Access-Control-Allow-Origin", "https://areyouhungry.vercel.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
